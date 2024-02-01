04cd1a9dfc8450fdd013d48751761c855ec0d72a
vehehgito <aryann2708@gmail.com>
2024-02-01 15:50:45 +0530

    Configured Routes @ Router/Router.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import Router from "./Components/Routers/Router";
 
 function App() {
-  return (
-    <>
-      <Router />
-    </>
-  );
+	return (
+		<>
+			<Router />
+		</>
+	);
 }
 
 export default App;
--- frontend/src/Components/Routers/Router.jsx
+++ frontend/src/Components/Routers/Router.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Route, Routes, createBrowserRouter } from 'react-router-dom'
+
 import StickyNavbar from '../Navbar/Navbar'
 import Dashboard from '../Dashboard/Dashboard'
 import Home from '../Home/Home'
+import Footer from '../Footer/Footer'
+import Error404 from '../Error/Error404'
 
 function Router() {
-  return (
-    <div>
-        <StickyNavbar />
-        {/* <Dashboard /> */}
-    </div>
-  )
+	return (
+		<div>
+			<StickyNavbar />
+			<Dashboard />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<Error404 />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default Router

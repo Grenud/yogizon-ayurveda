51086b2367bb8fa9fc2b91005341646cf259acae
vehehgito <aryann2708@gmail.com>
2024-02-03 13:59:14 +0530

    Added BhumiVeda logo

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -6,25 +6,25 @@ import Footer from "../Footer/Footer";
 import FAQs from "./FAQs";
 
 function Home() {
-  return (
-    <div>
-      <div>
-        <StickyNavbar />
-      </div>
-      <div className="">
-        <Hero />
-      </div>
-      <div className="">
-        <Features />
-      </div>
-      <div>
-        <FAQs />
-      </div>
-      <div className="mt-[10rem]">
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<StickyNavbar />
+			</div>
+			<div className="">
+				<Hero />
+			</div>
+			<div className="">
+				<Features />
+			</div>
+			<div>
+				<FAQs />
+			</div>
+			<div className="mt-[10rem]">
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default Home;

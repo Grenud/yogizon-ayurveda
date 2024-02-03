e2863ad3cdd2221ce7e4297a7232132fc277940b
vehehgito <aryann2708@gmail.com>
2024-02-03 14:34:01 +0530

    Fixed MegaMenu

--- frontend/src/Components/MegaMenu/MegaMenu.jsx
+++ frontend/src/Components/MegaMenu/MegaMenu.jsx
@@ -1,227 +1,214 @@
 import React from "react";
 import {
-    Navbar,
-    Collapse,
-    Typography,
-    IconButton,
-    List,
-    ListItem,
-    Menu,
-    MenuHandler,
-    MenuList,
-    MenuItem,
+	Navbar,
+	Collapse,
+	Typography,
+	IconButton,
+	List,
+	ListItem,
+	Menu,
+	MenuHandler,
+	MenuList,
+	MenuItem,
+	Button,
 } from "@material-tailwind/react";
 import {
-    ChevronDownIcon,
-    Bars3Icon,
-    XMarkIcon,
+	ChevronDownIcon,
+	Bars3Icon,
+	XMarkIcon,
 } from "@heroicons/react/24/outline";
 import {
-    Bars4Icon,
-    GlobeAmericasIcon,
-    NewspaperIcon,
-    PhoneIcon,
-    RectangleGroupIcon,
-    SquaresPlusIcon,
-    SunIcon,
-    TagIcon,
-    UserGroupIcon,
+	Bars4Icon,
+	PhoneIcon,
+	SquaresPlusIcon,
+	SunIcon,
+	UserGroupIcon,
 } from "@heroicons/react/24/solid";
 
 import leaf from "../../assets/leaf.png";
 
-
 const navListMenuItems = [
-    {
-        title: "Our Philosophy",
-        description: "Learn about our mission and values.",
-        icon: SquaresPlusIcon,
-    },
-    {
-        title: "Gallery",
-        description: "View our gallery of photos and videos.",
-        icon: UserGroupIcon,
-    },
-    {
-        title: "Customer Feedback",
-        description: "Read what our customers are saying about us.",
-        icon: Bars4Icon,
-    },
-    {
-        title: "Research",
-        description: "Explore our research and development initiatives.",
-        icon: SunIcon,
-    },
-    {
-        title: "Contact Us",
-        description: "Find the perfect solution for your needs.",
-        icon: PhoneIcon,
-    }
+	{
+		title: "Our Philosophy",
+		description: "Learn about our mission and values.",
+		icon: SquaresPlusIcon,
+	},
+	{
+		title: "Gallery",
+		description: "View our gallery of photos and videos.",
+		icon: UserGroupIcon,
+	},
+	{
+		title: "Customer Feedback",
+		description: "Read what our customers are saying about us.",
+		icon: Bars4Icon,
+	},
+	{
+		title: "Research",
+		description: "Explore our research and development initiatives.",
+		icon: SunIcon,
+	},
+	{
+		title: "Contact Us",
+		description: "Find the perfect solution for your needs.",
+		icon: PhoneIcon,
+	},
 ];
 
 function NavListMenu() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
-    const renderItems = navListMenuItems.map(
-        ({ icon, title, description }, key) => (
-            <a href="#" key={key}>
-                <MenuItem className="flex items-center gap-3 rounded-lg">
-                    <div className="flex items-center justify-center rounded-lg !bg-blue-gray-50 p-2 ">
-                        {" "}
-                        {React.createElement(icon, {
-                            strokeWidth: 2,
-                            className: "h-6 text-gray-900 w-6",
-                        })}
-                    </div>
-                    <div>
-                        <Typography
-                            variant="h6"
-                            color="blue-gray"
-                            className="flex items-center text-sm font-bold"
-                        >
-                            {title}
-                        </Typography>
-                        <Typography
-                            variant="paragraph"
-                            className="text-xs !font-medium text-blue-gray-500"
-                        >
-                            {description}
-                        </Typography>
-                    </div>
-                </MenuItem>
-            </a>
-        ),
-    );
+	const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+	const renderItems = navListMenuItems.map(
+		({ icon, title, description }, key) => (
+			<a href="#" key={key}>
+				<MenuItem className="flex items-center gap-3 rounded-lg">
+					<div className="flex items-center justify-center rounded-lg !bg-blue-gray-50 p-2 ">
+						{" "}
+						{React.createElement(icon, {
+							strokeWidth: 2,
+							className: "h-6 text-gray-900 w-6",
+						})}
+					</div>
+					<div>
+						<Typography
+							variant="h6"
+							color="blue-gray"
+							className="flex items-center text-sm font-bold"
+						>
+							{title}
+						</Typography>
+						<Typography
+							variant="paragraph"
+							className="text-xs !font-medium text-blue-gray-500"
+						>
+							{description}
+						</Typography>
+					</div>
+				</MenuItem>
+			</a>
+		)
+	);
 
-    return (
-        <React.Fragment>
-            <Menu
-                open={isMenuOpen}
-                handler={setIsMenuOpen}
-                offset={{ mainAxis: 20 }}
-                placement="bottom"
-                allowHover={true}
-            >
-                <MenuHandler>
-                    <Typography as="div" variant="small" className="font-medium">
-                        <ListItem
-                            className="flex items-center gap-2 py-2 pr-4 font-medium text-gray-900"
-                            selected={isMenuOpen || isMobileMenuOpen}
-                            onClick={() => setIsMobileMenuOpen((cur) => !cur)}
-                        >
-                            About Us
-                            <ChevronDownIcon
-                                strokeWidth={2.5}
-                                className={`hidden h-3 w-3 transition-transform lg:block ${isMenuOpen ? "rotate-180" : ""
-                                    }`}
-                            />
-                            <ChevronDownIcon
-                                strokeWidth={2.5}
-                                className={`block h-3 w-3 transition-transform lg:hidden ${isMobileMenuOpen ? "rotate-180" : ""
-                                    }`}
-                            />
-                        </ListItem>
-                    </Typography>
-                </MenuHandler>
-                <MenuList className="hidden max-w-screen-xl rounded-xl lg:block">
-                    <ul className="grid grid-cols-3 gap-y-2 outline-none outline-0">
-                        {renderItems}
-                    </ul>
-                </MenuList>
-            </Menu>
-            <div className="block lg:hidden">
-                <Collapse open={isMobileMenuOpen}>{renderItems}</Collapse>
-            </div>
-        </React.Fragment>
-    );
+	return (
+		<React.Fragment>
+			<Menu
+				open={isMenuOpen}
+				handler={setIsMenuOpen}
+				offset={{ mainAxis: 20 }}
+				placement="bottom"
+				allowHover={true}
+			>
+				<MenuHandler>
+					<Typography
+						as="div"
+						variant="small"
+						className="font-medium"
+					>
+						<ListItem
+							className="flex items-center gap-2 py-2 pr-4 font-medium text-gray-900"
+							selected={isMenuOpen || isMobileMenuOpen}
+							onClick={() => setIsMobileMenuOpen((cur) => !cur)}
+						>
+							About Us
+							<ChevronDownIcon
+								strokeWidth={2.5}
+								className={`hidden h-3 w-3 transition-transform lg:block ${
+									isMenuOpen ? "rotate-180" : ""
+								}`}
+							/>
+							<ChevronDownIcon
+								strokeWidth={2.5}
+								className={`block h-3 w-3 transition-transform lg:hidden ${
+									isMobileMenuOpen ? "rotate-180" : ""
+								}`}
+							/>
+						</ListItem>
+					</Typography>
+				</MenuHandler>
+				<MenuList className="hidden max-w-screen-xl rounded-xl lg:block">
+					<ul className="grid grid-cols-3 gap-y-2 outline-none outline-0">
+						{renderItems}
+					</ul>
+				</MenuList>
+			</Menu>
+			<div className="block lg:hidden">
+				<Collapse open={isMobileMenuOpen}>{renderItems}</Collapse>
+			</div>
+		</React.Fragment>
+	);
 }
 
 function NavList() {
-    return (
-        <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
-            <Typography
-                as="a"
-                href="#"
-                variant="small"
-                color="blue-gray"
-                className="font-medium"
-            >
-                <ListItem className="flex items-center gap-2 py-2 pr-4">Home</ListItem>
-            </Typography>
+	return (
+		<List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
+            
 
-            <NavListMenu />
-
-            <Typography
-                as="a"
-                href="#"
-                variant="small"
-                color="blue-gray"
-                className="font-medium"
-            >
-                <ListItem className="flex items-center gap-2 py-2 pr-4">
-                    Sign Up
-                </ListItem>
-            </Typography>
-            <Typography
-                as="a"
-                href="#"
-                variant="small"
-                color="blue-gray"
-                className="font-medium"
-            >
-                <ListItem className="flex items-center gap-2 py-2 pr-4">
-                    Sign In
-                </ListItem>
-            </Typography>
-        </List>
-    );
+			<NavListMenu />
+			<Button
+				variant="gradient"
+				size="sm"
+				color="green"
+				className="hidden lg:inline-block"
+			>
+				<span>Sign Up</span>
+			</Button>
+			<Button
+				variant="text"
+				size="sm"
+				color="green"
+				className="hidden lg:inline-block"
+			>
+				<span>Sign In</span>
+			</Button>
+		</List>
+	);
 }
 
 export default function MegaMenuWithHover() {
-    const [openNav, setOpenNav] = React.useState(false);
+	const [openNav, setOpenNav] = React.useState(false);
 
-    React.useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth >= 960 && setOpenNav(false),
-        );
-    }, []);
+	React.useEffect(() => {
+		window.addEventListener(
+			"resize",
+			() => window.innerWidth >= 960 && setOpenNav(false)
+		);
+	}, []);
 
-    return (
-        <Navbar className="mx-auto max-w-screen-xl px-4 py-2">
-            <div className="flex items-center justify-between text-blue-gray-900">
-                <div className="flex items-center justify-center">
-                    <img src={leaf} alt="leaf" className="h-6 w-6" />
-                    <Typography
-                        as="a"
-                        href="#"
-                        variant="h6"
-                        color="green"
-                        textGradient
-                        className="mr-4 cursor-pointer py-1.5 lg:ml-2"
-                    >
-                        ayurveda
-                    </Typography>
-                </div>
-                <div className="hidden lg:block">
-                    <NavList />
-                </div>
-                <IconButton
-                    variant="text"
-                    color="blue-gray"
-                    className="lg:hidden"
-                    onClick={() => setOpenNav(!openNav)}
-                >
-                    {openNav ? (
-                        <XMarkIcon className="h-6 w-6" strokeWidth={2} />
-                    ) : (
-                        <Bars3Icon className="h-6 w-6" strokeWidth={2} />
-                    )}
-                </IconButton>
-            </div>
-            <Collapse open={openNav}>
-                <NavList />
-            </Collapse>
-        </Navbar>
-    );
+	return (
+		<Navbar className="mx-auto max-w-screen-xl px-4 py-2 fixed top-0 left-0 right-0 z-[100]">
+			<div className="flex items-center justify-between text-blue-gray-900">
+				<div className="flex items-center justify-center">
+					<img src={leaf} alt="leaf" className="h-6 w-6" />
+					<Typography
+						as="a"
+						href="#"
+						variant="h6"
+						color="green"
+						textGradient
+						className="mr-4 cursor-pointer py-1.5 lg:ml-2"
+					>
+						ayurveda
+					</Typography>
+				</div>
+				<div className="hidden lg:block">
+					<NavList />
+				</div>
+				<IconButton
+					variant="text"
+					color="blue-gray"
+					className="lg:hidden"
+					onClick={() => setOpenNav(!openNav)}
+				>
+					{openNav ? (
+						<XMarkIcon className="h-6 w-6" strokeWidth={2} />
+					) : (
+						<Bars3Icon className="h-6 w-6" strokeWidth={2} />
+					)}
+				</IconButton>
+			</div>
+			<Collapse open={openNav}>
+				<NavList />
+			</Collapse>
+		</Navbar>
+	);
 }

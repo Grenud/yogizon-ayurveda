51943f092634e20ce72ada52a869019ff4ddbeb6
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-02-06 13:54:35 +0530

    Login Modal Setup

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,57 +1,71 @@
-import React from "react";
-import { Input, Button, Typography } from "@material-tailwind/react";
+import React from 'react'
 import {
-	CogIcon,
-	UserIcon,
-	BuildingLibraryIcon,
-} from "@heroicons/react/24/outline";
+	Typography,
+	Button,
+	Dialog,
+	Card,
+	CardBody,
+	Input,
+	Checkbox,
+	CardFooter,
+} from "@material-tailwind/react";
 
-import Footer from "../Footer/Footer";
-import MegaMenuWithHover from "../Navbar/MegaMenu";
 
-function Login() {
-
-	return (
-		<div>
-			<MegaMenuWithHover />
-			<div className="min-h-screen w-full px-[17rem] py-4 flex flex-col justify-center items-center gap-7">
-				<div className="flex flex-col items-center justify-center gap-5">
-					<Typography variant="h3" color="green">
-						Login
-					</Typography>
-					<Typography color="gray">
-						Complete the form to login.
-					</Typography>
-
-                    <form className="mt-[7rem]">
-                        <div className="flex flex-col justify-center items-center gap-7 w-full">
-                            <div className="w-[50rem]">
-                                <Input type="email" label="Email" />
-                            </div>
-                            <div className="w-[50rem]">
-                                <Input type="password" label="Password" />
-                            </div>
-                        </div>
-
-                        <div className="mt-5 flex justify-between w-full">
-                                <Button color="green">
-                                    Login
-                                </Button>
-                            
-                        </div>
-				</form>
-
-				</div>
-
-				<form className="mt-[7rem]">
-					
-
-					
-				</form>
-			</div>
-			<Footer />
-		</div>
-	);
+const Login = ({loginOpen, handleLoginOpen}) => {
+  return (
+	<>
+		<Dialog
+        size="xs"
+        open={loginOpen}
+        handler={handleLoginOpen}
+        className="bg-transparent shadow-none"
+      >
+        <Card className="mx-auto w-full max-w-[24rem]">
+          <CardBody className="flex flex-col gap-4">
+            <Typography variant="h4" color="blue-gray">
+              Sign In
+            </Typography>
+            <Typography
+              className="mb-3 font-normal"
+              variant="paragraph"
+              color="gray"
+            >
+              Enter your email and password to Sign In.
+            </Typography>
+            <Typography className="-mb-2" variant="h6">
+              Your Email
+            </Typography>
+            <Input label="Email" size="lg" />
+            <Typography className="-mb-2" variant="h6">
+              Your Password
+            </Typography>
+            <Input label="Password" size="lg" />
+            <div className="-ml-2.5 -mt-3">
+              <Checkbox label="Remember Me" />
+            </div>
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Button variant="gradient" onClick={handleLoginOpen} fullWidth>
+              Sign In
+            </Button>
+            <Typography variant="small" className="mt-4 flex justify-center">
+              Don&apos;t have an account?
+              <Typography
+                as="a"
+                href="#signup"
+                variant="small"
+                color="blue-gray"
+                className="ml-1 font-bold"
+                onClick={handleLoginOpen}
+              >
+                Sign up
+              </Typography>
+            </Typography>
+          </CardFooter>
+        </Card>
+      </Dialog>
+	</>
+  )
 }
 
-export default Login;
+export default Login
